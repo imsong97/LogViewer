40fb3647691184220e1049d44d346449ba6dae43
Yun-Ho SONG <imsong1997@gmail.com>
2026-06-18 20:55:30 +0900

    git ignore

--- composeApp/build/generated/compose/resourceGenerator/kotlin/desktopMainResourceCollectors/logviewer/composeapp/generated/resources/ActualResourceCollectors.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/desktopMainResourceCollectors/logviewer/composeapp/generated/resources/ActualResourceCollectors.kt
@@ -18,6 +18,7 @@ internal actual val Res.allDrawableResources: Map<String, DrawableResource> by l
 
 internal actual val Res.allStringResources: Map<String, StringResource> by lazy {
   val map = mutableMapOf<String, StringResource>()
+  _collectDesktopMainString0Resources(map)
   return@lazy map
 }
 
